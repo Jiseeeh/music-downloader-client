5d55c704a1b8613691858336061f1684214e5278
Jiseeeh <johncarlo.camara1@gmail.com>
2023-08-09 20:26:31 +0800

    feat: add initial components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,49 @@
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+
+import { Header } from "@/components/header/Header";
+import { HeaderImage } from "@/components/header/HeaderImage";
+import { TypographyH3 } from "@/components/typography/TypographyH3";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import { DownloadForm } from "@/components/Download";
+
 export default function Home() {
-  return <main>HEY</main>;
+  return (
+    <section className="min-h-screen">
+      <nav className="flex max-w-5xl mx-auto">
+        <Sheet>
+          <SheetTrigger>
+            <HamburgerMenuIcon
+              className="p-2 cursor-pointer"
+              width={50}
+              height={50}
+            />
+          </SheetTrigger>
+          <SheetContent side={"left"}>
+            <SheetHeader>
+              <SheetTitle>Recently used links</SheetTitle>
+              <SheetDescription>
+                These are the links you have used recently.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+        <section className="p-2 ml-auto flex items-center">
+          <HeaderImage height={40} width={40} />
+          <TypographyH3 content="Downloader" />
+        </section>
+      </nav>
+      <section>
+        <Header />
+        <DownloadForm />
+      </section>
+    </section>
+  );
 }
